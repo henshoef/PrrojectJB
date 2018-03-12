85ab9e49b4ac1def5b66f9c5dcddc617a29c2700
henshoef <36810075+henshoef@users.noreply.github.com>
2018-03-12 15:43:39 +0200

    v4

--- main/java/com/example/android/moviemanagment/AddMovie.java
+++ main/java/com/example/android/moviemanagment/AddMovie.java
@@ -1,5 +1,6 @@
 package com.example.android.moviemanagment;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,33 +8,46 @@ import android.view.View;
 import android.widget.EditText;
 
 public class AddMovie extends AppCompatActivity {
-//EditText title=findViewById(R.id.title);
-    Intent intent=new Intent();
+    //EditText title=findViewById(R.id.title);
+    String state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_movie);
     }
-    public void onClickOk(View v){
 
-        EditText title=findViewById(R.id.title);
-        EditText description=findViewById(R.id.descrition);
-        EditText url=findViewById(R.id.url);
+    public void onClickOk(View v) {
+        Intent intent = getIntent();
+        state = intent.getStringExtra("state");
+        EditText title = findViewById(R.id.title);
+        EditText description = findViewById(R.id.descrition);
+        EditText url = findViewById(R.id.url);
+        if (state.equals("add")) {
+            // state=intent.getStringExtra("state");
+            String movieName = title.getText().toString();
+            String desc = description.getText().toString();
+            String imageUrl = url.getText().toString();
 
-       String movieName= title.getText().toString();
-       String desc=description.getText().toString();
-       String imageUrl=url.getText().toString();
+            intent.putExtra("name", movieName);
+            intent.putExtra("description", desc);
+            intent.putExtra("url", imageUrl);
+            setResult(RESULT_OK, intent);
 
-       intent.putExtra("name",movieName);
-        intent.putExtra("description",desc);
-        intent.putExtra("url",imageUrl);
-
-    setResult(RESULT_OK,intent);
-    finish();
+        } else {
+            String movieName = intent.getStringExtra("MovieName");
+            String desc = intent.getStringExtra("Desc");
+            String imageUrl = intent.getStringExtra("ur");
+            title.setText(movieName + "");
+            description.setText(desc + "");
+            url.setText(imageUrl + "");
+        }
+        finish();
     }
-    public void onClickCancel(View v){
-        setResult(RESULT_CANCELED,intent);
+
+    public void onClickCancel(View v) {
+        Intent intent = new Intent();
+        setResult(RESULT_CANCELED, intent);
         finish();
     }
 }
--- main/java/com/example/android/moviemanagment/MainActivity.java
+++ main/java/com/example/android/moviemanagment/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -15,144 +16,125 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<MovieSample> movies = new ArrayList<MovieSample>();
-    private String movieName;
-    private String movieDesc;
-    private String movieUrl;
-
-
+    private String movieName = "";
+    private String movieDesc = "";
+    private String movieUrl = "";
+    private int counter = 1;
+    private int sizeW = 0;
+    private int sizeH = 0;
     private MenuItem addMovieByMenual;
     private MenuItem addMovieByIntenet;
     private MenuItem deleteAll;
     private MenuItem exit;
-private final int requsetCode=1;
+    private final int requsetCode = 1;
+    private LinearLayout mainLayout;
+    private ArrayList<MovieSample> Movies;
+    private LinearLayout.LayoutParams buttonDetails;
+    private MovieSample movieSample;
+    private String state = "add";
+    private int resultCode;
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         //creating a Layout and setting it's background color
-        LinearLayout mainLayout = findViewById(R.id.mainLayout);
-
-
+        mainLayout = findViewById(R.id.mainLayout);
         //creating a Button Array
-        ArrayList<MovieSample> Movie = new ArrayList<MovieSample>();
-
-
-
-
-
+        Movies = new ArrayList<MovieSample>();
         //here we set the height and width of the button and EditText to match their content
-        LinearLayout.LayoutParams buttonDetails = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
+        buttonDetails = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
-
-
-        //Give rules to position the
-
-
-
-
-
-
-        //setting the EditText size by dp casted to px ("setWidth" takes only px)
+        //setting the EditText sizeW by dp casted to px ("setWidth" takes only px)
         Resources r = getResources();
-        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,
-                r.getDisplayMetrics()
-        );
-
-
-
-
-
+        sizeW = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200,
+                r.getDisplayMetrics());
+        sizeH = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300,
+                r.getDisplayMetrics());
 
     }
 
-
-
-
     public boolean onCreateOptionsMenu(Menu menu) {
-
         MenuInflater menuInflater = getMenuInflater();
-
         menuInflater.inflate(R.menu.main, menu);
-
-
-
         // Need to find menu items only here because they belong to the menu and not to the main layout.
-
         // Also, they are not created yet on the onCreate event:
-
         addMovieByMenual = menu.findItem(R.id.addMovieByManual);
         addMovieByIntenet = menu.findItem(R.id.addMovieByInternet);
         deleteAll = menu.findItem(R.id.deleteAll);
-
         exit = menu.findItem(R.id.exit);
-
-
-
         return true;
-
     }
 
     // Return true to state that the menu event has been handled.
-
     // Return false to state that the menu event has not been handled.
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requsetCode == 1 && resultCode == RESULT_OK) {
+            state = "add";
+            movieName = data.getStringExtra("name");
+            movieDesc = data.getStringExtra("description");
+            movieUrl = data.getStringExtra("url");
+            movieSample = new MovieSample(this, counter, movieName, movieDesc, movieUrl);
+            movieSample.setId(counter);
+            movieSample.setText(movieName);
+            movieSample.setTextColor(getResources().getColor(R.color.white));
+            movieSample.setBackgroundResource(R.drawable.buttons);
+            movieSample.setWidth(sizeW);
+            movieSample.setHeight(sizeH);
+            movieSample.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    state = "edit";
+                    Intent i = new Intent(MainActivity.this, AddMovie.class);
+                    i.putExtra("state", state);
+                    i.putExtra("name", movieName);
+                    i.putExtra("description", movieDesc);
+                    i.putExtra("url", movieUrl);
+                    startActivityForResult(i, 2);
+                    onActivityResultEdit(2,resultCode,i);
+                }
+            });
+            movies.add(movieSample);
+            mainLayout.addView(movieSample, buttonDetails);
+        }
+    }
+    protected void onActivityResultEdit(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+
+    }
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
-
         int itemId = item.getItemId();
-
-
-
-        if(itemId == R.id.addMovieByManual || itemId == R.id.deleteAll || itemId == R.id.exit||itemId == R.id.addMovieByManual) {
+        if (itemId == R.id.addMovieByManual || itemId == R.id.deleteAll || itemId == R.id.exit || itemId == R.id.addMovieByManual) {
 
             addMovieByMenual.setChecked(false);
             addMovieByIntenet.setChecked(false);
             deleteAll.setChecked(false);
             exit.setChecked(false);
             item.setChecked(true);
-
         }
-
-
-
         switch (itemId) {
-
-
-
             case R.id.addMovieByManual:
-                Intent intent=new Intent(this,AddMovie.class);
-               startActivityForResult(intent,requsetCode);
-
+                Intent intent = new Intent(this, AddMovie.class);
+                intent.putExtra("state", state);
+                startActivityForResult(intent, requsetCode);
+                onActivityResult(requsetCode, resultCode, intent);
+                state = "add";
+                counter++;
                 return true;
             case R.id.addMovieByInternet:
-
-
-
                 return true;
             case R.id.deleteAll:
-
                 return true;
-
             case R.id.exit:
-         finish();
-
+                finish();
                 return true;
-
-
         }
-
-
-
+        setContentView(mainLayout);
         return false;
-
     }
 
-
 }
-
